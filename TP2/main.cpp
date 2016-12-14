bb3410f595bbff6fbf2139439245ab818fe3c080
Juscelino Tanaka <juscelino.tanaka@gmail.com>
2016-12-14 18:48:03 -0400

    Initial Commit

---
+++ TP2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP2
+//
+//  Created by Juscelino Tanaka on 14/12/16.
+//  Copyright © 2016 Juscelino Tanaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
